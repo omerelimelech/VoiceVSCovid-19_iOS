2d120cfc1a1da2ee824824b4f9df4f28837acf43
Omer Elimelech <omere@globalbit.co.il>
2020-04-05 16:01:52 +0300

    Initial Commit

---
+++ Covid19VOCAAiTests/Covid19VOCAAiTests.swift
@@ -0,0 +1,34 @@
+//
+//  Covid19VOCAAiTests.swift
+//  Covid19VOCAAiTests
+//
+//  Created by Omer Elimelech on 05/04/2020.
+//  Copyright © 2020 Omer Elimelech. All rights reserved.
+//
+
+import XCTest
+@testable import Covid19VOCAAi
+
+class Covid19VOCAAiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
